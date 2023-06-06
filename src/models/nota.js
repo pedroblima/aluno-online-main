72d36d3b4a92ec9631e93c31483dc02d5f959143
pedroblima <pedrob@iesb.edu.br>
2023-06-06 19:59:52 -0300

    mediaCA

--- src/models/nota.js
+++ src/models/nota.js
@@ -18,20 +18,20 @@ class Nota {
   
     mediaCA() {
       const media = this.mediaFinal();
-      if (media >= 9.0) {
-        return "SS"; 
-      } else if (media >= 7.0 && media <= 8.99) {
-        return "MS"; 
-      } else if (media >= 5.0 && media <= 6.99) {
-        return "MM"; 
-      } else if (media >= 3.0 && media <= 4.99) {
-        return "MI"; 
-      } else if (media >= 0.1 && media <= 2.99) {
-        return "II"; 
-      } else {
-        return "SR"; 
-      }
-    }
+
+      if (media >= 9.0 && media <= 10) {
+          return "SS - Superior";
+      } else if (media >= 7.0 && media <= 8.9) {
+          return "MS - Médio Superior";
+      } else if (media >= 5.001 && media <= 6.9) {
+          return "MM - Médio";
+      } else if (media >= 3.0 && media <= 5) {
+          return "MI - Médio Inferior";
+      } else if (media >= 0.1 && media <= 2.9) {
+          return "II - Inferior";
+      };
+  }
+    
   }
   
   module.exports = Nota;
